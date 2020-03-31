802cec3289d5c57eddd474a67e18d736844a7d3b
Pavel Prikhodko <pavlikozzchannel@gmail.com>
2020-03-31 15:43:31 +0700

    Update

--- Lesson1/MyCalc/src/main/java/ru/sibsutis/App.java
+++ Lesson1/MyCalc/src/main/java/ru/sibsutis/App.java
@@ -1,46 +1,51 @@
 package ru.sibsutis;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Simple Calculator");
-        Scanner sc = new Scanner(System.in);
-        double var = 0;
-        System.out.println("Put x");
-        double X = sc.nextDouble();
-        System.out.println("Put operation");
-        sc.nextLine();
-        String O = sc.nextLine();
-        System.out.println("Put y");
-        double Y = sc.nextDouble();
-        switch (O) {
-            case "+":
-                var = X + Y;
-                break;
-            case "-":
-                var = X - Y;
-                break;
-            case "*":
-                var = X * Y;
-                break;
-            case "/":
-                if (Y == 0) {
-                    System.out.println("Error /0");
-                    break;
-                } else {
-                    var = X / Y;
-                    break;
-                }
-            case "^":
-                var = 1;
-                for (int i = 0; i < Y; i++) {
-                    var *= X;
-                }
-            default:
-                System.out.println("Input Error");
+        var sc = new Scanner(System.in);
+        Integer a, b, res = null;
+        String op;
+
+        try {
+            a = sc.nextInt();
+            op = sc.next();
+            b = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Input Error");
+            return;
         }
-        System.out.println("Var = " + var);
+
+        switch (op) {
+            case ("+"):
+                res = a + b;
+                break;
+            case ("-"):
+                res = a - b;
+                break;
+            case ("*"):
+                res = a * b;
+                break;
+            case ("/"):
+                if (b != 0) {
+                    res = a / b;
+                } else {
+                    System.out.println("You can't divide by 0");
+                }
+                break;
+            case ("^"):
+                res = 1;
+                for (int i = 0; i < b; i++) {
+                    res *= a;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+        System.out.printf("%d %s %d = %d", a, op, b, res);
     }
 
 }
